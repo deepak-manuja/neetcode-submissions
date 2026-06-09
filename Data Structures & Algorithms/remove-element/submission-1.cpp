a78c370a2b5153bddf79d398a6eff2a0858c837c
Deepak Manuja <deepak1910@protonmail.com>
2026-06-09 14:41:02 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=val)
+            {
+                swap(nums[i],nums[index]);
+                index++;
+            }
+        }
+
+        return index;
+    }
+};
